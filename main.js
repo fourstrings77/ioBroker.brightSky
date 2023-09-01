f38d321f1442e42608c5ab4f66c97194ec12a652
Hauke Hagedorn <hagedorn.hauke@gmx.de>
2023-09-01 21:10:34 +0200

    add onUnload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const iobroker = require('@iobroker/adapter-core');
 const axios = require('axios');
+const adapterName = require('./package.json').split('.').pop();
 
 const API_BASE_URL = 'https://api.brightsky.dev/weather' //querystring: ?lat=52&lon=7.6&date=2020-04-21'
 
@@ -8,19 +9,25 @@ class brightsky extends iobroker.Adapter{
     constructor(options){
         super({
             ...options,
-            name: 'brightsky'
+            name: adapterName
         });
         this.on('ready', this.onReady.bind(this))
+        this.on('unload', this.onUnload.bind(this))
        
     }
     async onReady(){
-        this._getLatLon();
+       const coords = this._getLatLon();
+
+       this.log.info(JSON.stringify(coords))
+
+    }
+    async onUnload(){
+
     }
     async _getLatLon() {
 
         try{
             const state = await this.getForeignObjectsAsync('system.config');
-            console.log(state.common.longitude);
             return [state.common.longitude, state.common.latitude]
         } catch(error){
             this.log.error(error);
